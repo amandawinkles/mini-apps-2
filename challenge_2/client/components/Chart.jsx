4665171a694a64ed14d8095806b29b9e36e3d859
Amanda Winkles <amandawinkles@gmail.com>
2020-10-29 22:50:46 -0500

    setup

---
+++ challenge_2/client/components/Chart.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Chart from 'chart.js';
+
+const Chart = (props) => (
+
+);
+
+export default Chart;
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
+const axios = require('axios');
+const port = 3000;
+const Chart = require('chart.js');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(express.static('./public'));
 
 
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
+
+module.exports = app;
