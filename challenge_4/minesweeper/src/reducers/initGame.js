748dcbfbb6dfdc2b07b802d9538bbee2c07562ed
Amanda Winkles <amandawinkles@gmail.com>
2020-12-17 14:41:03 -0600

    reducers WIP

--- challenge_4/minesweeper/src/reducers/initGame.js
+++ challenge_4/minesweeper/src/reducers/initGame.js
@@ -1,8 +1,29 @@
 import Redux from 'redux';
+import { INIT } from '../actions/initGame.js';
 
-const initGameReducer = (state = { board: [], win: false, lose: false }, action) => {
+const initGameReducer = (state = { board: [], boardSize: 10, win: false, lose: false }, action) => {
   switch (action.type) {
     case 'INIT': {
+      //create board/matrix
+      const board = Array(10).fill(0).map(() =>
+        Array(10).fill(0)
+      );
+      const rows = board.length;
+      const cols = board[0].length;
+      //place 10 bombs randomly on new board
+      let bombs = 0;
+      while (bombs < 10) {
+        //select random row & column junction
+        const randRow = Math.floor(Math.random() * 10);
+        const randCol = Math.floor(Math.random() * 10);
+        //place bomb if space = 0
+        if (board[randRow][randCol] === 0) {
+          board[randRow][randCol] = 10;
+          //increase bombs count
+          bombs++;
+        }
+      }
+      //return board
       return action.boardSetup || null;
     default:
       return state;
